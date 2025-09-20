ed4237bb64fe9bca782a8ca76584064edd6d3caa
Eze <eze.gonzalez@vera.com.uy>
2025-09-19 22:55:55 -0300

    medio kalman

--- c_sim/main.c
+++ c_sim/main.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <math.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,6 +7,36 @@
 #include "bateria.h"
 #include "emf.h"
 #include "matrix.h"
+
+typedef struct {
+    size_t len, capacity;
+    double *data;
+} DArray;
+
+DArray DArray_new() {
+    DArray v = {0, 10, malloc(sizeof(double) * 10)};
+
+    if (!v.data) {
+        puts("oops");
+        exit(1);
+    }
+
+    return v;
+}
+
+void DArray_push(DArray *this, double x) {
+    if (this->len == this->capacity) {
+        this->capacity *= 2;
+        this->data = malloc(sizeof(double) * this->capacity);
+        if (!this->data) {
+            puts("oops");
+            exit(1);
+        }
+    }
+
+    this->data[this->len] = x;
+    this->len += 1;
+}
 
 void dump_to_file() {
     FILE *f = fopen("data.txt", "w");
@@ -75,6 +106,93 @@ void test() {
     }
 }
 
+void print_matrix(Matrix A) {
+    for (size_t i = 0; i < A.fil; ++i) {
+        for (size_t j = 0; j < A.col; ++j)
+            printf("%f ", A.mat[i][j]);
+        puts("");
+    }
+}
+
 int main() {
+    constexpr int cant_muestras = BAT_N;
+    double muestreo_deseado = 1. / 3600.;
+
+    double bat_ci = .1;
+    double capacidad = 7.7; // A/h
+
+    double tension_min = 3.05;
+
+    double a = 9.25e-02;
+    double b = 3.8125e-02;
+    double r = 5.125e-02;
+    Matrix A = {
+        2,
+        2,
+        {{1, 0}, {1 - exp(-muestreo_deseado / b), exp(-muestreo_deseado / b)}},
+    };
+    Matrix B = {
+        1,
+        2,
+        {{-1, (b - a) * (1 - exp(-muestreo_deseado / b)) - 1}},
+    };
+    B = scalar_mult(muestreo_deseado / capacidad, B);
+
+    for (int i = 0; i < cant_muestras; ++i) {
+        corriente[i] *= -1;
+        voltaje[i] /= 1'000;
+    }
+
+    // observador
+    double R2 = 1.;
+    Matrix Pb = {2, 2, {{1, 0}, {0, 1}}};
+    Matrix Xb = {1, 2, {{bat_ci, bat_ci}}};
+
+    double jk = 1.;
+    double Vhat[cant_muestras] = {};
+    double Vh[cant_muestras] = {};
+    Vhat[0] = inverse_emf(bat_ci);
+
+    for (int EKF = 0; EKF < 2; ++EKF) {
+        double xxmin[cant_muestras] = {};
+        double TT[cant_muestras] = {};
+        double TE[cant_muestras] = {};
+        double Se[cant_muestras] = {};
+        double XXe[cant_muestras] = {};
+        double Ps[cant_muestras] = {};
+        double Px[cant_muestras] = {};
+        double J[2] = {};
+        DArray ne = DArray_new();
+        DArray e = DArray_new();
+
+        for (int i = 0; i < cant_muestras; ++i) {
+            double Xm = emf(voltaje[i] + corriente[i] * r);
+            double df = derivative_emf(Xb.mat[0][1]);
+
+            if (!EKF) {
+                Matrix R1 = {2, 2, {{1, 0}, {0, 10}}};
+                Matrix C = {1, 2, {{0, 1}}};
+
+                Matrix K = scalar_mult(
+                    (1. / R2 +
+                     (matrix_mult(matrix_mult(C, Pb), transpose(C))).mat[0][0]),
+                    matrix_mult(Pb, transpose(C)));
+
+                Matrix Xe = matrix_sum(Xb, scalar_mult(Xm - Xb.mat[0][1], K));
+
+                Matrix P = inverse(matrix_sum(
+                    inverse(Pb), scalar_div(R2, matrix_mult(transpose(C), C))));
+
+                Xb = matrix_sum(matrix_mult(A, Xe),
+                                scalar_mult(corriente[i], B));
+                Pb = matrix_sum(matrix_mult(matrix_mult(A, P), transpose(A)),
+                                R1);
+
+                Vh[i] = inverse_emf(Xb.mat[0][1]) - corriente[i] * r;
+            } else {
+            }
+        }
+    }
+
     return 0;
 }
